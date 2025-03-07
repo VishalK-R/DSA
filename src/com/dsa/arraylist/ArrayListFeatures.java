dda3bd98bb76043663b518cf70b577f6f28eef3e
Vishal-KR31 <bruhath.vishal@outlook.com>
2025-03-07 15:31:57 +0530

    Initializing and Declaration of ArrayList

---
+++ src/com/dsa/arraylist/ArrayListFeatures.java
@@ -0,0 +1,23 @@
+package com.dsa.arraylist;
+
+import java.util.ArrayList;
+
+public class ArrayListFeatures {
+
+	public static void main(String[] args) {
+		// Declaring and initializing arrayList with initial capacity
+		ArrayList <Integer> ar = new ArrayList<>(10);
+		ar.add(1);
+		ar.add(2);
+		ar.add(3);
+		ar.add(4);
+		ar.add(5);
+		ar.add(6);
+		ar.add(7);
+		ar.add(8);
+		ar.add(9);
+		ar.add(10);
+		System.out.println(ar);
+	}
+
+}
