a27a3ba4a1d444e968a54d67426e69cb575f9f49
Vishal-KR31 <bruhath.vishal@outlook.com>
2025-04-10 14:05:25 +0530

    ArraysLeetCode - Easy Part 3 Solutions for: [Cells with Odd Values in a Matrix](https://leetcode.com/problems/cells-with-odd-values-in-a-matrix/) [Matrix Diagonal Sum](https://leetcode.com/problems/matrix-diagonal-sum/) [Find Numbers with Even Number of Digits](https://leetcode.com/problems/find-numbers-with-even-number-of-digits/) [Transpose Matrix](https://leetcode.com/problems/transpose-matrix/) [Add to Array-Form of Integer](https://leetcode.com/problems/add-to-array-form-of-integer/) [Maximum Population Year](https://leetcode.com/problems/maximum-population-year/)

---
+++ src/leet/arrays/easy/MaxPopulationYear.java
@@ -0,0 +1,44 @@
+package leet.arrays.easy;
+
+public class MaxPopulationYear {
+
+	/**You are given a 2D integer array logs where each 
+	 * logs[i] = [birthi, deathi] indicates the birth and death years 
+	 * of the ith person.
+	 * 
+	 * The population of some year x is the number of people alive 
+	 * during that year. The ith person is counted in year x's 
+	 * population if x is in the inclusive range [birthi, deathi - 1]. 
+	 * Note that the person is not counted in the year that they die.
+	 * 
+	 * Return the earliest year with the maximum population.
+	 */
+	public static void main(String[] args) {
+		int [][] logs = {{1950,1961},{1960,1971},{1970,1981}};
+		System.out.println(maximumPopulation(logs));
+
+	}
+	
+	public static int maximumPopulation(int[][] logs) {
+        int [] countPerYear = new int[101];
+        for (int i = 0; i < logs.length; i++) {
+			int j =logs[i][0];
+			int k =logs[i][1];
+			while(j<k) {
+				countPerYear[j-1950]++;
+				j++;
+			}
+		}
+        
+        int maxPopulation = 0;
+        int year = 1950;
+        for (int i = 0; i < countPerYear.length; i++) {
+			if(countPerYear[i]>maxPopulation) {
+				maxPopulation=countPerYear[i];
+				year = i+1950;
+			}
+		}
+        return year;
+    }
+
+}
