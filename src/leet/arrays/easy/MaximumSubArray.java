741033d1ca141a604060d9138176ddbb4d2461cf
Vishal-KR31 <bruhath.vishal@outlook.com>
2025-04-14 14:13:39 +0530

    ArraysLeetCode - Easy Part 5 Solutions for

---
+++ src/leet/arrays/easy/MaximumSubArray.java
@@ -0,0 +1,29 @@
+package leet.arrays.easy;
+
+public class MaximumSubArray {
+
+	/**Given an integer array nums, find the subarray with the largest sum, 
+	 * and return its sum.
+	 * 
+	 */
+	public static void main(String[] args) {
+		int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+		System.out.println(maxSubArray(nums));
+	}
+
+	public static int maxSubArray(int[] nums) {
+        int sum = nums[0];
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+        	sum+=nums[i];
+        	if(sum<nums[i]) {
+        		sum=nums[i];
+        	}
+        	if(sum>max) {
+        		max=sum;
+        	}
+        	
+		}
+        return max;
+    }
+}
---
+++ src/leet/arrays/easy/ReshapeTheMatrix.java
@@ -0,0 +1,47 @@
+package leet.arrays.easy;
+
+import java.util.Arrays;
+
+public class ReshapeTheMatrix {
+
+	/**
+	 * In MATLAB, there is a handy function called reshape which can reshape an m x
+	 * n matrix into a new one with a different size r x c keeping its original
+	 * data.
+	 * 
+	 * You are given an m x n matrix mat and two integers r and c representing the
+	 * number of rows and the number of columns of the wanted reshaped matrix.
+	 * 
+	 * The reshaped matrix should be filled with all the elements of the original
+	 * matrix in the same row-traversing order as they were.
+	 */
+	public static void main(String[] args) {
+		int[][] mat = { { 1, 2 }, { 3, 4 } };
+		int r = 2, c = 2;
+		Arrays.stream(matrixReshape(mat, r, c)).map(Arrays::toString).forEach(System.out::println);
+
+	}
+
+	public static int[][] matrixReshape(int[][] mat, int r, int c) {
+		int matRows = mat.length;
+		int matCols = mat[0].length;
+		if (matRows * matCols != r * c)
+			return mat;
+		int[][] reshapedMatrix = new int[r][c];
+		int row = 0, col = 0;
+		for (int i = 0; i < r; i++) {
+			for (int j = 0; j < c; j++) {
+				reshapedMatrix[i][j] = mat[row][col];
+				if (col == matCols - 1) {
+					row++;
+					col = 0;
+				} else {
+					col++;
+				}
+			}
+		}
+
+		return reshapedMatrix;
+	}
+
+}
